9d532cd1e6d6099c35b4b6d4a1599995bfb2d532
Amir Tawfik <taamer@gmail.com>
2022-06-11 21:40:42 -0400

    updated wedding details

--- src/components/details_card/index.jsx
+++ src/components/details_card/index.jsx
@@ -1,14 +1,21 @@
 const DetailsCard = () => {
     return (
         <div className='details-card'>
-            <center><p><h3> September 23, 2022 </h3>
-            at half past five in the evening </p>
-            <p><h3> Jericho Terrace </h3>
-            249 Jericho Turnpike 
-            Mineola, NY 11501 </p>
-            <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.668795547271!2d-73.647735248881!3d40.747312843289585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c287f8f3df7e2f%3A0xa718abfb21237036!2sJericho%20Terrace!5e0!3m2!1sen!2sus!4v1654562468776!5m2!1sen!2sus"></iframe></p>
-            <p><h3> Attire </h3>
-            Formal </p></center>
+            <center>
+                <h2>September 23, 2022</h2>
+                <p>at half past five in the evening </p>
+
+                <h2>Jericho Terrace</h2>
+                <p>249 Jericho Turnpike 
+                Mineola, NY 11501 </p>
+
+                <div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.668795547271!2d-73.647735248881!3d40.747312843289585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c287f8f3df7e2f%3A0xa718abfb21237036!2sJericho%20Terrace!5e0!3m2!1sen!2sus!4v1654562468776!5m2!1sen!2sus"></iframe>
+                </div>
+
+                <h2>Attire</h2>
+                <p>Formal </p>
+            </center>
         </div>
     );
 }
